40f3208e9745a44a1a6e62043c922fe21b9a4195
Augustogus01 <augusto.rodas@ieee.org>
2023-06-17 19:24:22 -0500

    Aqui intente correr el source en visual
    no corrio primer intento

--- Secuencia_leds/src/main.cpp
+++ Secuencia_leds/src/main.cpp
@@ -6,7 +6,8 @@ const int16_t LED_AMARILLO=3;
 const int16_t LED_ROJO=4;
 const int16_t SW1=10;
 /*******************************************************************************/
-bool apagarLeds(int16_t leds[],int16_t len);
+bool apagarLeds(int16_t* leds,int16_t len);
+
 void setup() {
   pinMode(LED_VERDE, OUTPUT);
   pinMode(LED_AMARILLO, OUTPUT);
@@ -16,14 +17,20 @@ void setup() {
 }
 
 void loop() {
-  int16_t leds[]={LED_VERDE,LED_AMARILLO,LED_ROJO};
-  int16_t len=sizeof(leds)/sizeof(int16_t);                                //Aqui se define la longitud del arreglo
+  int16_t arregloLeds[]={LED_VERDE,LED_AMARILLO,LED_ROJO};
+  int16_t len=sizeof(arregloLeds)/sizeof(int16_t);                                //Aqui se define la longitud del arreglo
   while (1) {
     for(int16_t i=0;i<len;i++){
-      apagarLeds(leds, len);
-      digitalWrite(leds[i],HIGH);
+      apagarLeds(arregloLeds, len);
+      digitalWrite(arregloLeds[i],HIGH);
       delay(500);
     }
   }
 }
 
+bool apagarLeds(int16_t* leds,int16_t len){
+  for(int16_t i=0;i<len;i++){
+    digitalWrite(leds[i],LOW);
+  }
+
+}
